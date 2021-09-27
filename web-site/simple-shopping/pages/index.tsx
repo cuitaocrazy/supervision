f67c0055a6d4595de4dd286359caf727541d25ca
cuitao <ctllfh@gmail.com>
2021-09-27 13:54:35 +0800

    add module

--- web-site/simple-shopping/pages/index.tsx
+++ web-site/simple-shopping/pages/index.tsx
@@ -1,7 +1,22 @@
+import useAxios from 'axios-hooks'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div className="bg-gray-500 text-white">Hello World!</div>
+  const [{ data, loading, error }, refetch] = useAxios(
+    '/api/hello'
+  )
+
+  return <div>
+    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+      onClick={() => refetch()}
+    >
+      Refresh
+    </button>
+    {/* <Button name="test" /> */}
+    <div className="bg-gray-500 text-white">{
+      loading ? "Loading..." : `Hello World!${data.name}`
+    }</div>
+  </div>
 }
 
 export default Home
