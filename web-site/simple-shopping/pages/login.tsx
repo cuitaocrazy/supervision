5ec9df39c310b2b229ca531a09c090d885ab5184
Kong Li <1263039973@qq.com>
2021-10-09 16:43:57 +0800

    修改登录页面：去掉注释的内容，form表单宽度固定，要素间的间距；配置文件添加只要颜色、次要颜色、错误颜色；去掉标点符号使整体统一。

--- web-site/simple-shopping/pages/login.tsx
+++ web-site/simple-shopping/pages/login.tsx
@@ -4,16 +4,16 @@ import { useRouter } from 'next/router'
 
 
 type FormData = {
-  username: string;
-  password: string;
+  username: string
+  password: string
 }
 
 const Login: NextPage = () => {
   const { register, handleSubmit, formState: { errors }, setValue } = useForm<FormData>()
-  const router = useRouter();
+  const router = useRouter()
 
   const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
-    saveLoginData();
+    saveLoginData()
   }
 
   //TODO 保存数据
@@ -26,48 +26,29 @@ const Login: NextPage = () => {
 
   }
 
-    // return <div className="flex flex-col h-screen bg-green-100 dark:bg-red-100">
-    //   <div className="grid mx-2 my-20 place-items-center sm:my-auto">
-    //     <div className="w-11/12 p-12 px-6 py-10 bg-white rounded-lg shadow-md sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 sm:px-10 sm:py-6 lg:shadow-lg">
-    //       <h2 className="text-3xl font-semibold text-center text-gray-800 lg:text-4xl">登录</h2>
-    //       <form className="mt-10" onSubmit={handleSubmit(onSubmit)}>
-    //         <label className="block text-xs font-semibold text-gray-600">用户名</label>
-    //         <input className="block w-full px-1 py-3 mt-2 text-gray-800 border-b-2 border-gray-100 appearance-none focus:text-gray-500 focus:outline-none focus:border-gray-200"
-    //           type="text"  {...register('username', { required: true, minLength: 2, maxLength: 10 },)} placeholder="请输入用户名" />
-    //         {errors.username && <p className="pt-2 pl-3 text-base text-red-500">用户名不能为空</p>}
+  return <div className="flex h-screen bg-green-100">
+  <form onSubmit={handleSubmit(onSubmit)} className="p-12 px-6 py-10 pt-4 mx-auto my-auto border-2 rounded-lg shadow-md border-primaryColor-300 w-80 bg-primaryColor-100">
+    <fieldset >
+      <legend className="mb-4 text-3xl font-semibold text-center text-gray-800">登录</legend>
+      <div>
+        <label className="block mb-2 text-xs font-semibold text-gray-600">用户名</label>
+        <input type="text" className="block w-full px-1 py-3 border-2 rounded-md bg-primaryColor-50 border-primaryColor-200 focus:bg-gray-500 focus:ring-opacity-20 focus:border-secondaryColor-500 focus:ring-offset-blue-300 focus:ring-4"
+          {...register('username', { required: true },)} placeholder="请输入用户名" />
+        {errors.username && <p className="pt-2 pl-3 text-base text-errorColor-500">用户名不能为空</p>}
+      </div>
+      <div className="mt-6">
+        <label className="block mb-2 text-xs font-semibold text-gray-600">密码</label>
+        <input type="text" className="block w-full px-1 py-3 border-2 rounded-md bg-primaryColor-50 border-primaryColor-200 focus:bg-gray-500 focus:ring-opacity-20 focus:border-secondaryColor-500 focus:ring-offset-blue-300 focus:ring-4"
+          {...register('password', { required: true })} placeholder="请输入密码" />
+        {errors.password && <p className="pt-2 pl-3 text-base text-errorColor-500">密码不能为空</p>}
+      </div>
+    </fieldset>
+    <input type="submit" className="w-full py-3 my-10 font-medium rounded-md shadow-md text-primaryColor-50 bg-secondaryColor-500 focus:outline-none hover:bg-secondaryColor-500 hover:shadow-none"
+      value="登录" />
+  </form>
+</div>
+}
 
-    //         <label className="block mt-2 text-xs font-semibold text-gray-600">密码</label>
-    //         <input className="block w-full px-1 py-3 mt-2 mb-4 text-gray-800 border-b-2 border-gray-100 appearance-none focus:text-gray-500 focus:outline-none focus:border-gray-200"
-    //           type="password" {...register('password', { required: true, minLength: 6, maxLength: 6 })} placeholder="请输入密码" />
-    //         {errors.password && <p className="">密码不能为空</p>}
 
-    //         <input className="w-full py-3 mt-10 font-medium text-white bg-green-300 rounded-md focus:outline-none hover:bg-green-500 hover:shadow-none"
-    //           type="submit" value="登录"  />
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
 
-  return <div className="flex h-screen bg-green-100 ">
-    <form onSubmit={handleSubmit(onSubmit)} className="w-11/12 p-12 px-6 py-10 pt-4 mx-auto my-auto bg-white rounded-lg shadow-md h-3/4 sm:w-1/2 md:w-2/5 xl:w-1/3 2xl:w-1/4 ">
-      <fieldset >
-        <legend className="mb-4 text-3xl font-semibold text-center text-gray-800">登录</legend>
-        <div>
-          <label className="block text-xs font-semibold text-gray-600">用户名</label>
-          <input type="text" className="block w-full px-1 py-3 text-gray-800 border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200 " {...register('username', { required: true},)} placeholder="请输入用户名"/>
-          {errors.username && <p className="pt-2 pl-3 text-base text-red-500">用户名不能为空</p>}
-        </div>
-        <div className="mt-2">
-          <label className="block text-xs font-semibold text-gray-600">密码</label>
-          <input type="text" className="block w-full px-1 py-3 text-gray-800 border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200 " {...register('password', { required: true})} placeholder="请输入密码" />
-          {errors.password && <p className="pt-2 pl-3 text-base text-red-500">密码不能为空</p>}
-        </div>
-      </fieldset>
-      <input type="submit" className="w-full py-3 my-10 font-medium text-white bg-green-300 rounded-md shadow-md focus:outline-none hover:bg-green-500 hover:shadow-none" value="登录" />
-    </form>
-  </div>
-   }
-
-  
-
-  export default Login
+export default Login
--- web-site/simple-shopping/tailwind.config.js
+++ web-site/simple-shopping/tailwind.config.js
@@ -1,7 +1,13 @@
+const colors=require('tailwindcss/colors')
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+      primaryColor:colors.gray,
+      secondaryColor:colors.blue,
+      errorColor:colors.red,
+    },
     extend: {},
   },
   variants: {
