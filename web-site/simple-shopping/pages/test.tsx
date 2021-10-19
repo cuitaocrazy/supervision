acd599728788fb98b6a44570ab8d15a82599ff5f
cuitao <ctllfh@gmail.com>
2021-10-19 14:46:07 +0800

    add order counter

--- web-site/simple-shopping/pages/test.tsx
+++ web-site/simple-shopping/pages/test.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
 import Layout from '../components/layout'
 import Card from '../components/Card'
+import { useAppDispatch } from '../app/hook'
+import { increment } from '../features/order-cart/counterSlice'
 
 const Test: NextPage = () => {
+  const dispatch = useAppDispatch()
   return <Layout title="测试">
     <div className="grid grid-cols-3 py-6 m-auto max-w-7xl">
-      <Card key="123" image="1.jpeg" title="Test" descript="test descript" amt={1} onAdd={ () => alert('123')}/>
+      <Card key="123" image="1.jpeg" title="Test" descript="test descript" amt={1} onAdd={ () => dispatch(increment())}/>
     </div>
   </Layout>
 }
