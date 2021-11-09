309609a4554296f603c8978af6c7fa61ffc498b4
Kong Li <1263039973@qq.com>
2021-11-09 11:24:13 +0800

    升级react、reactdom、next,SWC 替换 Terser 进行压缩，提高编译速度

--- web-site/simple-shopping/pages/index.tsx
+++ web-site/simple-shopping/pages/index.tsx
@@ -1,21 +1,7 @@
-import useAxios from 'axios-hooks'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const [{ data, loading }, refetch] = useAxios(
-    '/api/hello',
-  )
-
   return <div>
-    <button className='px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700'
-      onClick={() => refetch()}
-    >
-      Refresh
-    </button>
-    {/* <Button name="test" /> */}
-    <div className="text-white bg-gray-500">{
-      loading ? 'Loading...' : `Hello World!${data.name}`
-    }</div>
   </div>
 }
 
