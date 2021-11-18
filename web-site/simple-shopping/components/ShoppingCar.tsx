940017719e90670162e90ca64b79f9db416adf3a
Kong Li <1263039973@qq.com>
2021-11-18 12:10:06 +0800

    调整购物车页面的样式

--- web-site/simple-shopping/components/ShoppingCar.tsx
+++ web-site/simple-shopping/components/ShoppingCar.tsx
@@ -31,28 +31,28 @@ const ShoppingCar: FC<CarProps> = (props) => {
       alert('取消成功')
     }
   }
-  return <div>
-  <div className="flex flex-row justify-center w-1/2 mb-5 bg-white border border-gray-200 rounded-lg shadow-md mx-80">
-    <div className="ml-2">
-      <input type="radio" name="option" className="mt-10 mr-8" onClick={choose}/>
-    </div>
-    <div className="ml-2">
-      <img src={props.image} className="h-24 rounded-lg w-36" />
-    </div>
-    <div className="flex flex-col w-72">
-      <div className="h-10 px-3 py-2 truncate w-72 text-primary-600">{props.title}</div>
-      <div className="flex flex-row ">
-        <div className="px-3 text-red-500">{props.amt}</div>
-        <input type="button" className="p-1 ml-40 text-white rounded-md shadow-md bg-secondary-500 hover:bg-secondary-700 hover:shadow-none" value="删除" onClick={() => { deleteFun() }} >
-          {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-primary-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-          </svg> */}
-        </input>
+  return <div className="flex justify-center">
+    <div className="flex flex-row w-1/2 mb-5 bg-white border border-gray-200 rounded-lg shadow-md">
+     <div className="ml-2">
+        <input type="radio" name="option" className="mt-10 mr-8" onClick={choose}/>
+      </div>
+      <div className="ml-2">
+        <img src={props.image} className="h-24 rounded-lg " />
+      </div>
+      <div className="flex flex-col">
+        <div className="h-10 px-3 py-2 truncate text-primary-600">{props.title}</div>
+        <div className="flex flex-row ">
+          <div className="px-3 text-red-500">{props.amt}</div>
+          <input type="button" className="p-1 ml-40 text-white rounded-md shadow-md bg-secondary-500 hover:bg-secondary-700 hover:shadow-none" value="删除" onClick={() => { deleteFun() }} >
+            {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-primary-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+            </svg> */}
+          </input>
+        </div>
       </div>
     </div>
   </div>
-</div>
 }
 
 export default ShoppingCar
