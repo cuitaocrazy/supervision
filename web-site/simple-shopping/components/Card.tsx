4d1744b6f845cdae5a52720a880d5d0f51346f05
Kong Li <1263039973@qq.com>
2021-12-16 15:49:14 +0800

    商品列表页面，card中的链接添加小手的样式

--- web-site/simple-shopping/components/Card.tsx
+++ web-site/simple-shopping/components/Card.tsx
@@ -18,7 +18,7 @@ const Card: FC<CardProps> = (props) => <div className="max-w-lg mx-auto">
         Router.push({ pathname: '/detail', query: { item: JSON.stringify(props.item) } })
       }
     }>
-      <img className="rounded-t-lg" src={props.image} alt="" />
+      <img className="rounded-t-lg cursor-pointer" src={props.image} alt="" />
     </a>
     <div className="flex flex-row justify-between">
       <div className="p-5">
@@ -27,11 +27,11 @@ const Card: FC<CardProps> = (props) => <div className="max-w-lg mx-auto">
             Router.push({ pathname: '/detail', query: { item: JSON.stringify(props.item) } })
           }
         }>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{props.title}</h5>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 cursor-pointer">{props.title}</h5>
         </a>
         <p className="w-64 mb-3 font-normal text-gray-700 truncate">{props.descript}</p>
       </div>
-      <a className="inline-flex items-center px-3 py-2 my-10 mr-4 text-sm font-medium text-center text-white rounded-lg bg-secondary-500 hover:bg-secondary-800 focus:ring-4 focus:ring-secondary-300"
+      <a className="inline-flex items-center px-3 py-2 my-10 mr-4 text-sm font-medium text-center text-white rounded-lg cursor-pointer bg-secondary-500 hover:bg-secondary-800 focus:ring-4 focus:ring-secondary-300"
         onClick={props.onAdd} >
           添加
       </a>
