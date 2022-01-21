6088743dc57b0c372f5ec85a149b5b0ccbaa27db
Kong Li <1263039973@qq.com>
2022-01-21 13:57:13 +0800

    调整登录页面样式

--- web-site/regulator-mag-ionic/components/pages/Login.tsx
+++ web-site/regulator-mag-ionic/components/pages/Login.tsx
@@ -40,19 +40,19 @@ const Login = () => {
           <legend className="mb-4 text-3xl font-semibold text-center text-gray-800">登录</legend>
           <div>
             <label className="block mb-2 text-sm font-semibold text-gray-600">用户名</label>
-            <input type="text" className="relative block w-full px-1 py-3 pl-4 text-sm bg-white border rounded-md border-primary-200 focus:outline-none focus:glow-secondary-500"
+            <input type="text" className="relative block w-full px-1 py-3 pl-4 text-base bg-white border rounded-md border-primary-200 focus:outline-none focus:glow-secondary-500"
               {...register('username', { required: true })} placeholder="请输入用户名" />
             {errors.username && <p className="pt-2 pl-3 text-base text-error-400">用户名不能为空</p>}
           </div>
           <div className="mt-6">
             <label className="block mb-2 text-sm font-semibold text-gray-600">密码</label>
-            <input type="password" className="relative block w-full px-1 py-3 pl-4 text-sm bg-white border rounded-md border-primary-200 focus:outline-none focus:glow-secondary-500"
+            <input type="password" className="relative block w-full px-1 py-3 pl-4 text-base bg-white border rounded-md border-primary-200 focus:outline-none focus:glow-secondary-500"
               {...register('password', { required: true })} placeholder="请输入密码" />
             {errors.password && <p className="pt-2 pl-3 text-base text-error-400">密码不能为空</p>}
           </div>
           <div className="mt-6">
             <label className="block mb-2 text-sm font-semibold text-gray-600">角色</label>
-            <select {...register('role', { required: true })} name="role" id="role" className='w-full p-3 border rounded-md'>
+            <select {...register('role', { required: true })} name="role" id="role" className='w-full p-3 text-base bg-white border rounded-md'>
               <option value="SV" selected>监管机构</option>
               <option value="USV">教育机构</option>
               <option value="Finance">金融机构</option>
@@ -61,7 +61,7 @@ const Login = () => {
             {errors.role && <p className="pt-2 pl-3 text-base text-error-400">角色不能为空</p>}
           </div>
         </fieldset>
-        <input type="submit" className="w-full py-3 my-10 text-sm font-medium text-white rounded-md shadow-md bg-secondary-500 focus:outline-none hover:bg-secondary-700 hover:shadow-none"
+        <input type="submit" className="w-full py-3 my-10 mb-1 text-base font-medium text-white rounded-md shadow-md bg-secondary-500 focus:outline-none hover:bg-secondary-700 hover:shadow-none"
           value="登录" />
       </form>
     </div>
