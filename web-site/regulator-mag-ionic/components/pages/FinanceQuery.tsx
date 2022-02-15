d5dc766457010cb19cf175da823ed72d58206120
Kong Li <1263039973@qq.com>
2022-02-15 09:44:38 +0800

    调整页面样式

--- web-site/regulator-mag-ionic/components/pages/FinanceQuery.tsx
+++ web-site/regulator-mag-ionic/components/pages/FinanceQuery.tsx
@@ -169,6 +169,29 @@ const FinanceQuery:React.FC =()=>{
                           <IonLabel className='flex h-12 p-2 font-bold text-center text-primary-600 w-28'>交易日期：</IonLabel>
                           <IonDatetime className="flex w-56 h-12 pt-2.5 font-bold text-center text-primary-600 bg-white rounded-md" value={queryInfo.SubscribeStartDate} name='TranDate' displayFormat='YYYYMMDD' onIonChange={e=>{setQueryInfo({...queryInfo,...{SubscribeStartDate:e.detail.value!}})}}></IonDatetime>
                         </IonCol>
+                        <IonCol className="flex ml-8">
+                          <IonLabel className='flex h-12 p-2 font-bold text-center text-primary-600 w-28'>教育机构：</IonLabel>
+                          <IonLabel className='flex w-56 h-12 pt-2.5 pl-20 font-bold text-center text-primary-600 bg-white rounded-md' onClick={()=>setQueryInfo({...queryInfo,...{isOpen:!queryInfo.isOpen}})}>{queryInfo.USVOrgName}</IonLabel>
+                          <IonPicker
+                              isOpen={queryInfo.isOpen}
+                              columns={[usvPickerColumn]}
+                              buttons={[
+                                {
+                                  text: "取消",
+                                  role: "cancel",
+                                  handler: value => {
+                                    // setQueryInfo({...queryInfo,...{isOpen:!queryInfo.isOpen}})
+                                  }
+                                },
+                                {
+                                  text: "确认",
+                                  handler: value => { 
+                                    setQueryInfo({...queryInfo,...{USVOrgID:value.USVOrg.value,USVOrgName:value.USVOrg.text,isOpen:!queryInfo.isOpen}})
+                                  }
+                                }
+                              ]}
+                            ></IonPicker>
+                        </IonCol>
                         <IonCol className="flex justify-center">
                           <button className="w-24 p-2 text-white rounded-md bg-secondary-500 hover:bg-secondary-700 focus:outline-none">查询</button>
                         </IonCol>
