1951c6c08c1793f03f10f7ec3d176a023fa4755d
kongli123 <kongli860329@163.com>
2022-03-15 14:36:57 +0800

    修改二维码支付页面

--- web-site/simple-shopping/pages/qrCode.tsx
+++ web-site/simple-shopping/pages/qrCode.tsx
@@ -6,6 +6,7 @@ import { io } from 'socket.io-client'
 import { decrement } from '@/features/order-cart/counterSlice'
 import { useAppDispatch } from '@/app/hook'
 import LayoutNoCar from '@/components/layout-nocar'
+// import style from '@/components/Button.module.css'
 
 /** 二维码支付页面 */
 const preOrderURL = 'http://localhost:3004/preOrder'
@@ -23,15 +24,17 @@ const qrCode:NextPage = () => {
   const router = useRouter()
   const { USVOrgID, USVItemID, USVItemName, USVItemDesc, TranAmt } = router.query
   const [state, setState] = useState({ codeUrl: '', BankTranID: '', BankID: '', SVOrgID: '', USVOrderNo: '', time: 15 * 60 })
-  const [time, setTime] = useState(15 * 60)
+  const [time, setTime] = useState(60)
 
   // const getUrl = (urlInfo:UrlInfo) => {
   //   const { codeUrl, BankTranID, USVOrderNo, BankID, SVOrgID } = urlInfo
   //   return codeUrl + '?BankTranID=' + BankTranID + '&BankID=' + BankID + '&SVOrgID=' + SVOrgID + '&USVOrderNo=' + USVOrderNo
   // }
 
-  const min = Math.floor(time / 60)
-  const second = time % 60
+  // const min = Math.floor(time / 60)
+  const second = (time === 60) ? 60 : time % 60
+
+  // const style= (time<=0):
 
   useEffect(() => {
     const socket = io(socketUrl)
@@ -57,7 +60,6 @@ const qrCode:NextPage = () => {
         // const urlInfo : UrlInfo = { codeUrl, BankTranID, USVOrderNo, BankID, SVOrgID }
 
         const { PayUrl, SubscribeID } = json
-        console.log(PayUrl)
         socket.emit('pay', SubscribeID)
         socket.on(SubscribeID + '_pay', () => {
           console.log('支付成功')
@@ -77,20 +79,24 @@ const qrCode:NextPage = () => {
     const interval = setInterval(() => setTime(time - 1), 1000)
     return () => clearInterval(interval)
   }, [time])
+
   return <LayoutNoCar title="支付">
-    <div className="flex">
       <div className="pb-8 mx-auto mt-16 bg-white rounded-md w-96">
         <div className="pt-8 mx-2 mb-4 text-3xl font-semibold text-center text-gray-800">XX银行</div>
-        <div className="m-1 text-lg text-center text-gray-600">订单提交成功，请扫码完成支付！</div>
-        <div className="m-1 font-bold text-center text-gray-600">订单总计:&nbsp;<span className="text-lg text-red-700">¥{TranAmt}</span>&nbsp;</div>
-        <div className="flex justify-center py-4 mx-20 border border-gray-200 rounded-md">
-          <QRCode value={state.codeUrl} renderAs="svg" size={200} imageSettings={{ src: 'https://static.zpao.com/favicon.png', height: 50, width: 50, excavate: true }}></QRCode>
-        </div>
+        <div className="m-1 text-lg text-center text-gray-700">订单提交成功，请扫码完成支付！</div>
+        <div className="m-1 font-bold text-center text-gray-800">订单总计:&nbsp;<span className="text-lg text-red-700">¥{TranAmt}</span>&nbsp;</div>
+          <section className="flex flex-col items-center justify-center bg-gray-100 h-96">
+            <div className="z-20 bg-gray-800 rounded-lg w-60 h-60 bg-opacity-90"></div>
+            <div className="absolute z-30">
+              <QRCode value={state.codeUrl} renderAs="svg" size={200} imageSettings={{ src: 'https://static.zpao.com/favicon.png', height: 50, width: 50, excavate: true }}></QRCode>
+            </div>
+            <div className="absolute z-10 flex items-center justify-center h-12 text-center text-red-500 bg-gray-300 w-60">二维码已过期</div>
+          </section>
+          {/* <button className={props.className || style.btn}></button> */}
         {/* <div>{getUrl(state)}</div> */}
         <div>{state.codeUrl}</div>
-        <div className="m-2 text-sm text-center text-gray-600">请您在&nbsp;<span className="text-green-700">{min}分{second}秒</span>&nbsp;内完成支付，否则订单会被自动取消</div>
+        <div className="m-2 text-sm text-center text-gray-700">请您在&nbsp;<span className="text-green-700">{second}秒</span>&nbsp;内完成支付，否则订单会被自动取消</div>
       </div>
-    </div>
   </LayoutNoCar>
 }
 
