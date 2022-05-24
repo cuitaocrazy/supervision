a183b1a37b07f06f90d2a120be2304fe5ae7b3dd
fengming.jiang <fengming.jiang@bjyada.com>
2022-05-24 10:07:55 +0800

    ionic 升级到 6

--- web-site/consumerdemo/components/AppShell.tsx
+++ web-site/consumerdemo/components/AppShell.tsx
@@ -1,7 +1,7 @@
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { IonReactRouter, } from '@ionic/react-router';
-import {  Route,Redirect   } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import Tabs from './pages/Tabs';
 import Menu from './pages/Menu';
@@ -9,36 +9,37 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 
 
-import {AppContextProvider} from '../appState'
+import { AppContextProvider } from '../appState'
 
-
+// 装载ionic样式，ionic6 必须执行。
+setupIonicReact();
 window.matchMedia("(prefers-color-scheme: dark)").addListener(async (status) => {
   try {
     await StatusBar.setStyle({
       style: status.matches ? Style.Dark : Style.Light,
     });
-  } catch {}
+  } catch { }
 });
 
 const AppShell = () => {
   return (
     <AppContextProvider>
-    <IonApp>
-      <IonReactRouter>
-        {/* <Menu></Menu> */}
+      <IonApp>
+        <IonReactRouter>
+          {/* <Menu></Menu> */}
 
-         <IonSplitPane contentId="main" className="space-y-16 bg-gray-300 shadow-lg rounded-tl-xl rounded-bl-xl">
+          <IonSplitPane contentId="main" className="space-y-16 bg-gray-300 shadow-lg rounded-tl-xl rounded-bl-xl">
             <Menu />
             <IonRouterOutlet id="main">
-            <Route path="/tabs" render={() => <Tabs />} />
-            <Route path="/login" render={() => <Login />} />
-            <Route path="/home" render={() => <Home />} />
-            <Route exact={true} path="/" render={() => <Redirect to="/tabs" />} />
+              <Route path="/tabs" render={() => <Tabs />} />
+              <Route path="/login" render={() => <Login />} />
+              <Route path="/home" render={() => <Home />} />
+              <Route exact={true} path="/" render={() => <Redirect to="/tabs" />} />
             </IonRouterOutlet>
           </IonSplitPane>
 
-      </IonReactRouter>
-    </IonApp>
+        </IonReactRouter>
+      </IonApp>
     </AppContextProvider>
   );
 };
--- web-site/consumerdemo/pages/_app.tsx
+++ web-site/consumerdemo/pages/_app.tsx
@@ -1,27 +1,18 @@
-// import '../styles/globals.css'
-// import type { AppProps } from 'next/app'
-// import 'tailwindcss/tailwind.css';
-// import '@ionic/react/css/core.css';
-// import '@ionic/react/css/padding.css';
-// import '@ionic/react/css/float-elements.css';
-// import '@ionic/react/css/text-alignment.css';
-// import '@ionic/react/css/text-transformation.css';
-// import '@ionic/react/css/flex-utils.css';
-// import '@ionic/react/css/display.css';
-
-// import '../styles/global.css';
-// import '../styles/variables.css';
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
 // export default MyApp
 import Head from 'next/head';
 import Script from 'next/script';
+import { AppProps } from 'next/app';
 import React, { FC } from "react";
 import 'tailwindcss/tailwind.css';
+/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
@@ -30,14 +21,10 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 import '../styles/global.css';
-interface Props {
-  Component:any,
-  pageProps:any
-}
-  
+
 console.log('xxxxxxxxxx')
 // function MyApp({ Component, pageProps }) {
-  const MyApp: FC<Props> = ({ Component, pageProps }) =>{
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
