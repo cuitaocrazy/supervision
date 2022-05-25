8db1ee1045b7278c4af3268077af212877eb9bc6
Kong Li <1263039973@qq.com>
2022-05-25 11:15:17 +0800

    调整首页精选推荐样式

--- web-site/consumerdemo/components/pages/First.tsx
+++ web-site/consumerdemo/components/pages/First.tsx
@@ -47,14 +47,17 @@ const First=()=>{
               </div>
 
                {/* 课程列表 */}
-               <div className='grid grid-cols-4 mx-3 mt-2'>
-                 <div className='col-span-3 text-sm font-bold text-gray-900'>
-                   <svg className="inline w-5 h-5 text-indigo-600"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="12" y1="5" x2="12" y2="19" /></svg>
-                   精选推荐
+               <div className='grid items-center grid-cols-6 mt-3'>
+                 <div className='col-span-5 text-sm font-bold text-gray-900'>
+                   <svg className="inline w-5 h-5 ml-1 text-indigo-600"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="4" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="12" y1="4" x2="12" y2="19" /></svg>
+                   <div className='inline'>精选推荐</div>
                  </div>
-                 <div className='flex text-xs text-gray-400 justify-self-end'>
+                 <div className='flex mb-1 mr-3 text-xs text-gray-400 justify-self-end'>
                    <a href="http://www.w3school.com.cn">
-                     <div>更多<svg className="inline w-3 h-3 text-gray-400 "  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="9 6 15 12 9 18" /></svg></div>
+                     <div>
+                       更多
+                       <svg className="inline w-3 h-3 mb-1 text-gray-400"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="9 6 15 12 9 18" /></svg>
+                    </div>
                    </a>
                  </div>
                </div>
