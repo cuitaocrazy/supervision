8145ef154cb05fefc4fcdf47517bd997a569aee0
Kong Li <1263039973@qq.com>
2022-05-25 14:14:34 +0800

    添加motion包，添加收到通知后-小红点的样式

--- web-site/consumerdemo/components/pages/First.tsx
+++ web-site/consumerdemo/components/pages/First.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Router from 'next/router'
 import {IonPage,IonHeader,IonToolbar,IonContent,IonTitle,IonSearchbar,IonImg,IonList,IonItem,IonThumbnail,IonLabel,IonAvatar,IonChip,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent}from "@ionic/react"
+import {motion} from 'framer-motion'
 
 type Item = {
   src: string;
@@ -16,7 +17,8 @@ const First=()=>{
       <div className='grid h-10 grid-cols-10 pt-2 font-medium text-center text-white bg-primary-600 margin-auto'>
         <div className='col-span-9'>教育资金监管平台</div>
         <div className='text-center'>
-        <svg className="w-4 h-5 pt-1 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />  <path d="M13.73 21a2 2 0 0 1-3.46 0" /></svg>
+        <svg className="w-5 h-6 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />  <path d="M13.73 21a2 2 0 0 1-3.46 0" /></svg>
+        <motion.div key="2" className={'absolute top-2 right-5 ' } animate={{ scale: [1, 1.2, 1] }} transition={{ duration: 0.2 }}><span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">2</span></motion.div>
         </div>
       </div>
     </IonHeader>
