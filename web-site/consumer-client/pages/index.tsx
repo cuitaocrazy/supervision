90f00c611dd8520bce9623b39f55b3f997ec3588
Kong Li <1263039973@qq.com>
2022-05-30 10:34:52 +0800

    去掉测试的消费者客户端

--- web-site/consumer-client/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return <div className="bg-gray-500 text-white">Hello World!</div>
-}
-
-export default Home
--- web-site/consumer-client/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
