5137b87124028c7182b78da054caea6d54c861c7
Kong Li <1263039973@qq.com>
2022-06-08 11:22:45 +0800

    课程详情页面添加课程大纲和评价内容

--- web-site/consumerdemo/components/pages/LessonDetail.tsx
+++ web-site/consumerdemo/components/pages/LessonDetail.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {IonPage,IonHeader,IonContent}from "@ionic/react"
-import {motion} from 'framer-motion'
 // import Schedule from '../Schedule'
 
 const LessonDetail=()=>{
@@ -9,8 +8,7 @@ const LessonDetail=()=>{
       <div className='grid h-10 grid-cols-10 pt-2 font-medium text-center text-white bg-primary-600 margin-auto'>
         <div className='col-span-9'>课程详情</div>
         <div className='text-center'>
-        <svg className="w-5 h-6 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />  <path d="M13.73 21a2 2 0 0 1-3.46 0" /></svg>
-        <motion.div key="2" className={'absolute top-2 right-5 ' } animate={{ scale: [1, 1.2, 1] }} transition={{ duration: 0.2 }}><span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">2</span></motion.div>
+        <svg className="w-5 h-6 text-white"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="5" cy="12" r="1" />  <circle cx="12" cy="12" r="1" />  <circle cx="19" cy="12" r="1" /></svg>
         </div>
       </div>
     </IonHeader>
@@ -27,13 +25,16 @@ const LessonDetail=()=>{
                   <svg className="w-5 h-5 text-primary-500"  viewBox="0 7 24 24"  fill="none"  stroke="currentColor"  strokeWidth="4"  strokeLinecap="round"  strokeLinejoin="round">  <line x1="5" y1="12" x2="19" y2="12" /></svg>
                 </div>
               </div>
-              <div className='flex justify-center grow'>课程大纲</div>
-              <div className='flex justify-center grow'>评价(148)</div>
+              <div className='flex justify-center border-b-2 grow hover:text-primary-600 hover:font-medium border-b-primary-600'>课程大纲</div>
+              <div className='flex justify-center grow hover:text-primary-600 hover:font-medium' >评价(14)</div>
             </div>
+            
+            
+            
             {/* 培训机构介绍 */}
-            <div className='px-3 py-1 mx-3 rounded-lg shadow-md'>
+            {/* <div className='px-3 py-1 mx-3 rounded-lg shadow-md'>
               <div className='text-lg font-bold'>小画家美术培训</div>
-              {/* 讲师 姓名 星星 */}
+             
               <div className='flex pt-1'>
                 <div className='text-sm text-gray-500'>讲师:</div>
                 <div className='text-sm'>&nbsp;&nbsp;李雷</div>
@@ -55,7 +56,7 @@ const LessonDetail=()=>{
                   </svg>
                  </div>                                                                                                                                           
               </div>
-              {/* 价格和课时 */}
+             
               <div className='flex pt-1 text-xs'>
                 <div className="text-red-500 ">¥888.00</div>
                 <div className='pl-3 text-gray-500'>56课时</div>
@@ -68,10 +69,10 @@ const LessonDetail=()=>{
                 <div className='text-gray-500'>电话：</div>
                 <div>010-87667890</div>
               </div>
-            </div>
+            </div> */}
               
-            {/* 学习里程碑   */}
-            <div className='px-2 pt-3 pb-4 mx-3 mt-2 text-xs rounded-lg shadow-md'>
+           
+            {/* <div className='px-2 pt-3 pb-4 mx-3 mt-2 text-xs rounded-lg shadow-md'>
               <div className='flex'>
                 <div className='font-bold tracking-wider text-primary-500'>张大宝</div>
                 <div className='font-bold text-gray-600'>学习里程碑</div>
@@ -83,23 +84,22 @@ const LessonDetail=()=>{
               </div>
               <div className="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700 mt-1">
                 <div className="bg-primary-600 h-1.5 rounded-full w-1/2" ></div>
-                {/* <div className="w-8 h-4 text-white rounded-full bg-primary-600" >50%</div> */}
               </div>
             </div>
 
-            {/* 课程介绍 */}
+            
             <div className='p-2 mx-3 mt-2 text-xs rounded-lg shadow-md'>
               <div className='text-sm font-bold text-gray-600'>课程介绍</div>
               <div className='pt-1 text-gray-500'>艺术教育是未来教育，是快乐教育</div>
             </div>
 
-            {/* 教师简介 */}
+            
             <div className='p-2 mx-3 mt-2 text-xs rounded-lg shadow-md'>
               <div className='text-sm font-bold text-gray-600'>教师简介</div>
               <div className='pt-1 leading-5 text-gray-500'>李雷，清华大学美术学院，学士、硕士，7年资深美育教研从业经验，20年媒体从业经历。7年资深美育教研工作中，李雷多次获得美术相关奖项：世界最高美术奖、中国美术金彩奖、徐悲鸿美术奖,7年资深美育教研从业经验，20年媒体从业经历。清华大学美术学院，学士、硕士，7年资深美育教研从业经验，20年媒体从业经历。7年资深美育教研工作中，李雷多次获得美术相关奖项：世界最高美术奖、中国美术金彩奖、徐悲鸿美术奖,7年资深美育教研从业经验，20年媒体从业经历。7年资深美育教研工作中，李雷多次获得美术相关奖项：世界最高美术奖、中国美术金彩奖、徐悲鸿美术奖7年资深美育教研从业经验，20年媒体从业经历。7年资深美育教研工作中，李雷多次获得美术相关奖项：世界最高美术奖、中国美术金彩奖、徐悲鸿美术奖......</div>
             </div>   
 
-            {/* 订单信息     */}
+            
             <div className='px-2 pt-3 pb-4 mx-3 mt-2 text-xs rounded-lg shadow-md'>
               <div className='flex leading-6'>
                 <div className='pr-3 text-gray-500'>订单编号</div>
@@ -137,9 +137,119 @@ const LessonDetail=()=>{
                 <div>实付金额：</div>
                 <div>¥710.40</div>
               </div>
+            </div> */}
+             
+            {/* 课程大纲 */}
+            {/* <div className='px-2 pt-4 pb-3 mx-3 mt-2 shadow-md'>
+              <div className='text-sm font-bold'>课程大纲</div>
+              <div className='pt-2 pb-2 pl-4 text-sm rounded-md text-third-600 bg-primary-50'>01.太阳（圆型 暖色调）</div>
+            </div> */}
 
-
+            <div className='px-2 pt-4 pb-3 mx-3 mt-2 mb-2 shadow-md'>
+              <div>
+                <div className='grid grid-cols-2 justify-items-stretch'>
+                          <div className='flex flex-auto text-xs font-bold text-gray-600 justify-self-start'>源远流长</div>
+                          <div className='grid w-full grid-flow-col pl-20'>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                          </div>
+                        </div>
+                        <div className='pt-2 text-sm text-gray-500'>一个故事一幅作品，孩子很喜欢，老师讲解生动有趣。</div>
+                        <div className='flex justify-between'>
+                          <div className='flex text-xs text-gray-500'>
+                            <div className='pr-1'>18天前</div>
+                            <div className='pr-1'>|</div>
+                            <div >已上课时22时评论</div>
+                          </div>
+                          <div className='flex text-xs text-gray-500 justify-self-end'>回复</div>
+                        </div> 
+              </div>
             </div>
+
+
+            <div className='px-2 pt-4 pb-3 mx-3 mt-2 mb-2 rounded-md shadow-lg'>
+              <div>
+                <div className='grid grid-cols-2 justify-items-stretch'>
+                          <div className='flex flex-auto text-xs font-bold text-gray-600 justify-self-start'>源远流长</div>
+                          <div className='grid w-full grid-flow-col pl-20'>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                          </div>
+                        </div>
+                        <div className='pt-2 text-sm text-gray-500'>一个故事一幅作品，孩子很喜欢，老师讲解生动有趣。</div>
+                        <div className='flex justify-between'>
+                          <div className='flex text-xs text-gray-500'>
+                            <div className='pr-1'>18天前</div>
+                            <div className='pr-1'>|</div>
+                            <div >已上课时22时评论</div>
+                          </div>
+                          <div className='flex text-xs text-gray-500 justify-self-end'>回复</div>
+                        </div> 
+              </div>
+            </div>
+
+
+            <div className='px-2 pt-4 pb-3 mx-3 mt-2 mb-2 shadow-md'>
+              <div>
+                <div className='grid grid-cols-2 justify-items-stretch'>
+                          <div className='flex flex-auto text-xs font-bold text-gray-600 justify-self-start'>源远流长</div>
+                          <div className='grid w-full grid-flow-col pl-20'>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                            <svg className="w-3 h-3 my-1 text-yellow-500 "  fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+                            </svg>
+                          </div>
+                        </div>
+                        <div className='pt-2 text-sm text-gray-500'>一个故事一幅作品，孩子很喜欢，老师讲解生动有趣。</div>
+                        <div className='flex justify-between'>
+                          <div className='flex text-xs text-gray-500'>
+                            <div className='pr-1'>18天前</div>
+                            <div className='pr-1'>|</div>
+                            <div >已上课时22时评论</div>
+                          </div>
+                          <div className='flex text-xs text-gray-500 justify-self-end'>回复</div>
+                          
+                        </div> 
+              </div>
+            </div>
+
         </div>
         
         {/* 底部菜单 */}
