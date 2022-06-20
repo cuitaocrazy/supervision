204348f68a45c266c450524137b17587d7ae0acd
Kong Li <1263039973@qq.com>
2022-06-20 14:36:38 +0800

    提取首页和课程列表页面的课程列表组件、搜索框组件

--- web-site/consumerdemo/components/pages/Home.tsx
+++ web-site/consumerdemo/components/pages/Home.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import { FC } from 'react';
 import { IonPage, IonHeader, IonContent } from "@ionic/react"
 import { motion } from 'framer-motion'
-import { useForm, SubmitHandler } from 'react-hook-form'
 import { useRouter } from 'next/router'
+import Search from '../Search'
 
-type FormData = {
-  name: string;
+
+// 首页课程列表card组件
+const LessonListCard = () => {
+  const router = useRouter()
+  return (
+    <a onClick={
+      () => {
+        router.push('/searchLessonDetail')
+      }
+    }>
+      <div className="flex flex-col max-w-sm mb-3 ml-3 mr-2 bg-white border-gray-200 rounded-lg shadow-md h-36">
+        <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
+        <div className="flex flex-col justify-center px-2 pt-1">
+          <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">幼小衔接</p>
+          <p className='pt-1 text-xs text-gray-400 truncate'>简介：让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生</p>
+          <p className="text-xs text-gray-400 truncate">地址：廊坊市安次区和平路荣益广场3层206</p>
+        </div>
+      </div>
+    </a>
+  )
 }
-
 // 首页
 const Home = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
-  const router = useRouter()
-  const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
-    router.push('/searchLessonList')
-  }
+
   return <IonPage>
     <IonHeader>
       <div className='grid h-10 grid-cols-10 pt-2 font-medium text-center text-white bg-primary-600 margin-auto'>
@@ -33,22 +46,7 @@ const Home = () => {
         <div className='bg-white'>
 
           {/* 搜索框 */}
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className='fixed left-0 right-0 pb-2 mt-3 bg-white'>
-              <div className="flex pt-3 font-mono text-xs ">
-                <input type="text" className="flex items-center justify-center h-8 pl-4 ml-3 text-sm text-gray-300 border border-gray-400 shadow-lg rounded-l-3xl grow focus:outline-none focus:glow-primary-600"
-                  {...register('name', { required: true })}
-                  placeholder="请输入搜索关键词" x-model="search" />
-                <button type="submit" className="flex items-center justify-center flex-none h-8 mr-3 bg-primary-600 rounded-r-3xl w-14 focus:outline-none hover:bg-primary-700 ">
-                  <svg className="w-5 h-5 text-white" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </form>
-
-          {/* 搜索框以下内容设置为滚动 */}
+          <Search />
           {/* 轮播图 */}
           <div className='pt-20 mx-3'>
             <img className="w-full h-32 rounded-lg cursor-pointer " src='http://placekitten.com/g/200/300' alt="" />
@@ -72,65 +70,7 @@ const Home = () => {
 
           <div>
             <div className="grid py-2 sm1:grid-cols-2 sm2:grid-cols-2 sm3:grid-cols-2">
-              <a onClick={
-                () => {
-                  router.push('/searchLessonDetail')
-                }
-              }>
-                <div className="flex flex-col max-w-sm mb-3 ml-3 mr-2 bg-white border-gray-200 rounded-lg shadow-md h-36">
-                  <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                  <div className="flex flex-col justify-center px-2 pt-1">
-                    <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">幼小衔接</p>
-                    <p className='pt-1 text-xs text-gray-400 truncate'>简介：让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生</p>
-                    <p className="text-xs text-gray-400 truncate">地址：廊坊市安次区和平路荣益广场3层206</p>
-                  </div>
-                </div>
-              </a>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-3 mr-2 bg-white border-gray-200 rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">幼小衔接</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>简介：让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生</p>
-                  <p className="text-xs text-gray-400 truncate">地址：廊坊市安次区和平路荣益广场3层206</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-3 mr-2 bg-white border-gray-200 rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">幼小衔接</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>简介：让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生</p>
-                  <p className="text-xs text-gray-400 truncate">地址：廊坊市安次区和平路荣益广场3层206</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-3 mr-2 bg-white border-gray-200 rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">幼小衔接</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>简介：让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生</p>
-                  <p className="text-xs text-gray-400 truncate">地址：廊坊市安次区和平路荣益广场3层206</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-3 mr-2 bg-white border-gray-200 rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">幼小衔接</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>简介：让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生</p>
-                  <p className="text-xs text-gray-400 truncate">地址：廊坊市安次区和平路荣益广场3层206</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-3 mr-2 bg-white border-gray-200 rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">幼小衔接</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>简介：让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生让孩子提前适应小学生</p>
-                  <p className="text-xs text-gray-400 truncate">地址：廊坊市安次区和平路荣益广场3层206</p>
-                </div>
-              </div>
+              <LessonListCard />
             </div>
           </div>
         </div>
--- web-site/consumerdemo/components/pages/SearchLessonList.tsx
+++ web-site/consumerdemo/components/pages/SearchLessonList.tsx
@@ -2,10 +2,43 @@ import React from 'react';
 import { IonPage, IonHeader, IonContent } from "@ionic/react"
 import { motion } from 'framer-motion'
 import { useRouter } from 'next/router'
+import Search from '../Search'
+
+const SearchlessonlistCard = () => {
+  const router = useRouter()
+  return (
+    <div className='mt-3 bg-white rounded-lg shadow-md '>
+      <div className='grid grid-cols-3 mx-2 h-28 rounde-xl'>
+        <a onClick={
+          () => {
+            router.push('/searchLessonDetail')
+          }
+        }>
+          <img className='w-24 h-24 col-span-1 mt-2 ml-1 rounded-xl' src="http://placekitten.com/g/200/300"></img>
+        </a>
+        <div className='flex flex-col col-span-2 mt-2 justify-items-start'>
+          <a onClick={
+            () => {
+              router.push('/searchLessonDetail')
+            }
+          }>
+            <div className='mt-2 text-sm'>思维逻辑小游戏</div>
+            <div className='mt-1 text-xs text-gray-500'>锻炼孩子动手能力，提高孩子思维能力，学习完课程可组装不同形状。</div>
+            <div className='flex gap-3 mt-3'>
+              <div className='text-xs font-bold text-remind-500'>¥3558.00</div>
+              <div className='text-xs text-gray-700'>36课时</div>
+              <div className='text-xs text-gray-700'>核桃编程</div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 // 首页查询课程列表页面
 const SearchLessonList = () => {
-  const router = useRouter()
+
   return <IonPage>
     <IonHeader>
       <div className='grid h-10 grid-cols-10 pt-2 font-medium text-center text-white bg-primary-600 margin-auto'>
@@ -23,109 +56,12 @@ const SearchLessonList = () => {
         <div className='bg-white'>
 
           {/* 搜索框 */}
-          <div className='fixed left-0 right-0 pb-2 mt-3 bg-white'>
-            <div className="flex pt-3 font-mono text-xs ">
-              <input type="search" className="flex items-center justify-center pl-2 ml-3 text-gray-800 border shadow-lg rounded-l-3xl grow focus:outline-none focus:glow-primary-600"
-                placeholder="请输入搜索关键词" x-model="search" />
-              <button type="submit" className="flex items-center justify-center flex-none h-8 mr-3 bg-primary-600 rounded-r-3xl w-14 focus:outline-none hover:bg-primary-700 ">
-                <svg className="w-5 h-5 text-white" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-
+          <Search />
           {/* 课程列表card */}
           <div className='mx-3 pt-14 '>
-            <div className='mt-3 bg-white rounded-lg shadow-md '>
-              <div className='grid grid-cols-3 mx-2 h-28 rounde-xl'>
-                <a onClick={
-                  () => {
-                    router.push('/searchLessonDetail')
-                  }
-                }>
-                  <img className='w-24 h-24 col-span-1 mt-2 ml-1 rounded-xl' src="http://placekitten.com/g/200/300"></img>
-                </a>
-                <div className='flex flex-col col-span-2 mt-2 justify-items-start'>
-                  <a onClick={
-                    () => {
-                      router.push('/searchLessonDetail')
-                    }
-                  }>
-                    <div className='mt-2 text-sm'>思维逻辑小游戏</div>
-                    <div className='mt-1 text-xs text-gray-500'>锻炼孩子动手能力，提高孩子思维能力，学习完课程可组装不同形状。</div>
-                    <div className='flex gap-3 mt-3'>
-                      <div className='text-xs font-bold text-remind-500'>¥3558.00</div>
-                      <div className='text-xs text-gray-700'>36课时</div>
-                      <div className='text-xs text-gray-700'>核桃编程</div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className='mt-3 rounded-lg shadow-md'>
-              <div className='grid grid-cols-3 mx-2 h-28 rounde-xl'>
-                <img className='w-24 h-24 col-span-1 mt-2 ml-1 rounded-xl' src="http://placekitten.com/g/200/300"></img>
-                <div className='flex flex-col col-span-2 mt-2 justify-items-start'>
-                  <div className='mt-2 text-sm'>思维逻辑小游戏</div>
-                  <div className='mt-1 text-xs text-gray-500'>锻炼孩子动手能力，提高孩子思维能力，学习完课程可组装不同形状。</div>
-                  <div className='flex gap-3 mt-3'>
-                    <div className='text-xs font-bold text-remind-500'>¥3558.00</div>
-                    <div className='text-xs text-gray-700'>36课时</div>
-                    <div className='text-xs text-gray-700'>核桃编程</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className='mt-3 rounded-lg shadow-md'>
-              <div className='grid grid-cols-3 mx-2 h-28 rounde-xl'>
-                <img className='w-24 h-24 col-span-1 mt-2 ml-1 rounded-xl' src="http://placekitten.com/g/200/300"></img>
-                <div className='flex flex-col col-span-2 mt-2 justify-items-start'>
-                  <div className='mt-2 text-sm'>思维逻辑小游戏</div>
-                  <div className='mt-1 text-xs text-gray-500'>锻炼孩子动手能力，提高孩子思维能力，学习完课程可组装不同形状。</div>
-                  <div className='flex gap-3 mt-3'>
-                    <div className='text-xs font-bold text-remind-500'>¥3558.00</div>
-                    <div className='text-xs text-gray-700'>36课时</div>
-                    <div className='text-xs text-gray-700'>核桃编程</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className='mt-3 rounded-lg shadow-md'>
-              <div className='grid grid-cols-3 mx-2 h-28 rounde-xl'>
-                <img className='w-24 h-24 col-span-1 mt-2 ml-1 rounded-xl' src="http://placekitten.com/g/200/300"></img>
-                <div className='flex flex-col col-span-2 mt-2 justify-items-start'>
-                  <div className='mt-2 text-sm'>思维逻辑小游戏</div>
-                  <div className='mt-1 text-xs text-gray-500'>锻炼孩子动手能力，提高孩子思维能力，学习完课程可组装不同形状。</div>
-                  <div className='flex gap-3 mt-3'>
-                    <div className='text-xs font-bold text-remind-500'>¥3558.00</div>
-                    <div className='text-xs text-gray-700'>36课时</div>
-                    <div className='text-xs text-gray-700'>核桃编程</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className='mt-3 rounded-lg shadow-md'>
-              <div className='grid grid-cols-3 mx-2 h-28 rounde-xl'>
-                <img className='w-24 h-24 col-span-1 mt-2 ml-1 rounded-xl' src="http://placekitten.com/g/200/300"></img>
-                <div className='flex flex-col col-span-2 mt-2 justify-items-start'>
-                  <div className='mt-2 text-sm'>思维逻辑小游戏</div>
-                  <div className='mt-1 text-xs text-gray-500'>锻炼孩子动手能力，提高孩子思维能力，学习完课程可组装不同形状。</div>
-                  <div className='flex gap-3 mt-3'>
-                    <div className='text-xs font-bold text-remind-500'>¥3558.00</div>
-                    <div className='text-xs text-gray-700'>36课时</div>
-                    <div className='text-xs text-gray-700'>核桃编程</div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <SearchlessonlistCard />
+            <SearchlessonlistCard />
           </div>
-
-
         </div>
       </div>
     </IonContent>
