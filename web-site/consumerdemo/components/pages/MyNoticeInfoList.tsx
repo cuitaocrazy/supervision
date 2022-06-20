5e851cd18b6849133c02f4ceab16457552754318
Kong Li <1263039973@qq.com>
2022-06-20 15:00:04 +0800

    提取公告信息组件

--- web-site/consumerdemo/components/pages/MyNoticeInfoList.tsx
+++ web-site/consumerdemo/components/pages/MyNoticeInfoList.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import { IonPage, IonHeader, IonContent } from '@ionic/react'
 
+// 公告信息组件
+const NoticeInfoListCard = () => {
+  return (
+    <div className='px-4 py-3 mx-2 my-2 text-sm rounded-md shadow-md'>
+      <div className='flex items-center justify-between'>
+        <div className='flex font-bold text-gray-900'>监管机构公告</div>
+        <div className='flex pr-1 text-gray-400'>2022/05/18</div>
+      </div>
+      <div className='leading-8 text-gray-400 truncate'>今日调整监管机构存放法则相关条例相关法则如下:今日调整监管机构存放法则相关条例相关法则如下</div>
+    </div>
+  )
+}
+
 // 公告信息
 const MyNoticeInfoList = () => {
   return <IonPage>
@@ -10,8 +23,9 @@ const MyNoticeInfoList = () => {
       </div>
     </IonHeader>
     <IonContent>
-      <div>
-        <div className='px-4 py-3 mx-2 my-2 text-sm rounded-md shadow-md'>
+        <NoticeInfoListCard />
+
+        {/* <div className='px-4 py-3 mx-2 my-2 text-sm rounded-md shadow-md'>
           <div className='flex items-center justify-between'>
             <div className='flex font-bold text-gray-900'>监管机构公告</div>
             <div className='flex pr-1 text-gray-400'>2022/05/18</div>
@@ -33,24 +47,7 @@ const MyNoticeInfoList = () => {
             <div className='flex pr-1 text-gray-400'>2022/05/18</div>
           </div>
           <div className='leading-8 text-gray-400 truncate'>今日调整监管机构存放法则相关条例相关法则如下:今日调整监管机构存放法则相关条例相关法则如下</div>
-        </div>
-
-        <div className='px-4 py-3 mx-2 my-2 text-sm rounded-md shadow-md'>
-          <div className='flex items-center justify-between'>
-            <div className='flex font-bold text-gray-900'>监管机构公告</div>
-            <div className='flex pr-1 text-gray-400'>2022/05/18</div>
-          </div>
-          <div className='leading-8 text-gray-400 truncate'>今日调整监管机构存放法则相关条例相关法则如下:今日调整监管机构存放法则相关条例相关法则如下</div>
-        </div>
-
-        <div className='px-4 py-3 mx-2 my-2 text-sm rounded-md shadow-md'>
-          <div className='flex items-center justify-between'>
-            <div className='flex font-bold text-gray-900'>监管机构公告</div>
-            <div className='flex pr-1 text-gray-400'>2022/05/18</div>
-          </div>
-          <div className='leading-8 text-gray-400 truncate'>今日调整监管机构存放法则相关条例相关法则如下:今日调整监管机构存放法则相关条例相关法则如下</div>
-        </div>
-      </div>
+        </div> */}
     </IonContent>
   </IonPage>
 }
