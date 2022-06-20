4806c3ace19b93be34957e86ef2d77bd0de976b7
Kong Li <1263039973@qq.com>
2022-06-20 14:45:33 +0800

    提取机构列表的机构列表组件

--- web-site/consumerdemo/components/pages/OrgList.tsx
+++ web-site/consumerdemo/components/pages/OrgList.tsx
@@ -10,6 +10,40 @@ type Item = {
 const items: Item[] = [{ src: 'http://placekitten.com/g/200/300', text: 'a picture of a cat' },
 ];
 
+// 机构列表card组件
+const OrgListCard = () => {
+  return (
+    <div className="flex flex-col max-w-sm mb-3 ml-2 mr-2 bg-white rounded-lg shadow-md h-36">
+      <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
+      <div className="flex flex-col justify-center px-2 pt-1">
+        <div className='flex items-center justify-center'>
+          <div className='flex flex-auto text-xs font-bold text-gray-900 truncate cursor-pointer justify-self-start'>核桃编程</div>
+          <div className='flex flex-auto justify-self-end'>
+            <svg className="w-3 h-3 m-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+            </svg>
+            <svg className="w-3 h-3 mt-1 mr-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+            </svg>
+            <svg className="w-3 h-3 mt-1 mr-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+            </svg>
+            <svg className="w-3 h-3 mt-1 mr-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+            </svg>
+            <svg className="w-3 h-3 mt-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+            </svg>
+          </div>
+        </div>
+        {/* <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">核桃编程</p> */}
+        <p className='pt-1 text-xs text-gray-400 truncate'>地址：廊坊市安次区和平路荣益广场3层206</p>
+        <p className="text-xs text-gray-400 truncate">电话：0316-1234569</p>
+      </div>
+    </div>
+  )
+}
+
 // 教育机构列表页面
 const OrgList = () => {
   const router = useRouter()
@@ -65,87 +99,9 @@ const OrgList = () => {
             </div>
           </div>
           {/* 机构列表card */}
-          <div>
             <div className="grid py-2 sm1:grid-cols-2 sm2:grid-cols-2 sm3:grid-cols-2">
-              <div className="flex flex-col max-w-sm mb-3 ml-2 mr-2 bg-white rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <div className='flex items-center justify-center'>
-                    <div className='flex flex-auto text-xs font-bold text-gray-900 truncate cursor-pointer justify-self-start'>核桃编程</div>
-                    <div className='flex flex-auto justify-self-end'>
-                      <svg className="w-3 h-3 m-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-                      </svg>
-                      <svg className="w-3 h-3 mt-1 mr-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-                      </svg>
-                      <svg className="w-3 h-3 mt-1 mr-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-                      </svg>
-                      <svg className="w-3 h-3 mt-1 mr-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-                      </svg>
-                      <svg className="w-3 h-3 mt-1 text-yellow-500 " fill="orange" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-                      </svg>
-                    </div>
-                  </div>
-
-                  {/* <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">核桃编程</p> */}
-                  <p className='pt-1 text-xs text-gray-400 truncate'>地址：廊坊市安次区和平路荣益广场3层206</p>
-                  <p className="text-xs text-gray-400 truncate">电话：0316-1234569</p>
-
-                </div>
-
-              </div>
-
-
-              <div className="flex flex-col max-w-sm mb-3 ml-2 mr-2 bg-white rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">核桃编程</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>地址：廊坊市安次区和平路荣益广场3层206</p>
-                  <p className="text-xs text-gray-400 truncate">电话：0316-1234569</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-2 mr-2 bg-white rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">核桃编程</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>地址：廊坊市安次区和平路荣益广场3层206</p>
-                  <p className="text-xs text-gray-400 truncate">电话：0316-1234569</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-2 mr-2 bg-white rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">核桃编程</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>地址：廊坊市安次区和平路荣益广场3层206</p>
-                  <p className="text-xs text-gray-400 truncate">电话：0316-1234569</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-2 mr-2 bg-white rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">核桃编程</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>地址：廊坊市安次区和平路荣益广场3层206</p>
-                  <p className="text-xs text-gray-400 truncate">电话：0316-1234569</p>
-                </div>
-              </div>
-
-              <div className="flex flex-col max-w-sm mb-3 ml-2 mr-2 bg-white rounded-lg shadow-md h-36">
-                <img className="h-20 rounded-t-lg cursor-pointer" src='http://placekitten.com/g/200/300' alt="" />
-                <div className="flex flex-col justify-center px-2 pt-1">
-                  <p className="text-xs font-bold text-gray-900 truncate cursor-pointer ">核桃编程</p>
-                  <p className='pt-1 text-xs text-gray-400 truncate'>地址：廊坊市安次区和平路荣益广场3层206</p>
-                  <p className="text-xs text-gray-400 truncate">电话：0316-1234569</p>
-                </div>
-              </div>
+              <OrgListCard />
             </div>
-          </div>
         </div>
       </div>
     </IonContent>
