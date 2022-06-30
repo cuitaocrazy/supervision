627bc0263367f7fde6d9b02b5cfd559f11b43561
fengming.jiang <fengming.jiang@bjyada.com>
2022-06-30 15:56:34 +0800

    补充react-route，修复一个模块路径大小写错误

--- web-site/regulator-mag-ionic/components/AppShell.tsx
+++ web-site/regulator-mag-ionic/components/AppShell.tsx
@@ -1,5 +1,4 @@
-
-import { IonApp, IonRouterOutlet, IonSplitPane,setupIonicReact } from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { StatusBar, Style } from '@capacitor/status-bar';
 
 import { IonReactRouter } from '@ionic/react-router';
@@ -7,34 +6,34 @@ import { Redirect, Route } from 'react-router-dom';
 import Menu from './Menu';
 
 import Login from './pages/Login';
-import {AppContextProvider} from '../appState'
+import { AppContextProvider } from '../appState';
 import Tabs from './pages/Tabs';
 import Announcement from './pages/Announcement/query';
-import {AnnouncementDetail} from './pages/Announcement/detail';
-import {AnnouncementEdit} from './pages/Announcement/edit';
+import { AnnouncementDetail } from './pages/Announcement/detail';
+import { AnnouncementEdit } from './pages/Announcement/edit';
 import Black from './pages/Black/query';
 import Attendance from './pages/Attendance/query';
-import {AttendanceDetail} from './pages/Attendance/detail';
+import { AttendanceDetail } from './pages/Attendance/detail';
 
 // import Balance from './Balance/query'
-import BaseInfo from './pages/baseInfo/query'
+import BaseInfo from './pages/baseInfo/query';
 
-import SuperVisorOrgQuery from './pages/SupervisorOrg/query'
-import SuperVisorOrgDetail from './pages/SupervisorOrg/detail'
-import SuperVisorOrgEdit from './pages/SupervisorOrg/edit'
-import Complaint from './pages/Complaint/query'
-import Contract from './pages/Contract/query'
-import {ContractDetail} from './pages/Contract/detail'
-import ContractNego from './pages/contractNego/query'
-import Lesson from './pages/lesson/Query'
-import LessonDetail from './pages/lesson/Detail'
-import OrgMag from './pages/orgMag/Query'
-import OrgMagDetail from './pages/orgMag/Detail'
-import OrgMagEdit from './pages/orgMag/Edit'
-import Teacher from './pages/Teacher/query'
-import Tranfer from './pages/TranFer/query'
-import TranferDetail from './pages/TranFer/detail'
-import TranferManul from './pages/TranFerManual/query'
+import SuperVisorOrgQuery from './pages/SupervisorOrg/query';
+import SuperVisorOrgDetail from './pages/SupervisorOrg/detail';
+import SuperVisorOrgEdit from './pages/SupervisorOrg/edit';
+import Complaint from './pages/Complaint/query';
+import Contract from './pages/Contract/query';
+import { ContractDetail } from './pages/Contract/detail';
+import ContractNego from './pages/contractNego/query';
+import Lesson from './pages/lesson/Query';
+import LessonDetail from './pages/lesson/Detail';
+import OrgMag from './pages/orgMag/Query';
+import OrgMagDetail from './pages/orgMag/Detail';
+import OrgMagEdit from './pages/orgMag/Edit';
+import Teacher from './pages/Teacher/Query';
+import Tranfer from './pages/TranFer/query';
+import TranferDetail from './pages/TranFer/detail';
+import TranferManul from './pages/TranFerManual/query';
 import TeacherDetail from './pages/Teacher/Detail';
 import ComplaintDetail from './pages/Complaint/detail';
 import Subject from './pages/statistics/Subject';
@@ -42,13 +41,11 @@ import OrgSum from './pages/statistics/OrgSum';
 import Student from './pages/statistics/Student';
 import Income from './pages/statistics/income';
 import ChangePwd from './pages/ChangePwd';
-import ComplaintStatistic from './pages/statistics/complaint'
-
-
+import ComplaintStatistic from './pages/statistics/complaint';
 
 // 装载ionic样式，ionic6 必须执行。
 setupIonicReact();
-window.matchMedia("(prefers-color-scheme: dark)").addListener(async (status) => {
+window.matchMedia('(prefers-color-scheme: dark)').addListener(async status => {
   try {
     await StatusBar.setStyle({
       style: status.matches ? Style.Dark : Style.Light,
@@ -61,45 +58,56 @@ const AppShell = () => {
     <AppContextProvider>
       <IonApp>
         <IonReactRouter>
-          <IonSplitPane contentId="main" className="space-y-16 bg-gray-300 shadow-lg rounded-tl-xl rounded-bl-xl">
+          <IonSplitPane
+            contentId="main"
+            className="space-y-16 bg-gray-300 shadow-lg rounded-tl-xl rounded-bl-xl"
+          >
             <Menu />
             <IonRouterOutlet id="main">
-            {/* <Route path="/tabs" render={() => <Tabs />} /> */}
-            <Route path="/tabs/announcement/query" component={Announcement} exact={true} />
-            <Route path="/tabs/announcement/detail" component={AnnouncementDetail} exact={true} />
-            <Route path="/tabs/announcement/edit" component={AnnouncementEdit} exact={true} />
-            <Route path="/tabs/attendance/query" component={Attendance} exact={true} />
-            <Route path="/tabs/attendance/detail" component={AttendanceDetail} exact={true} />
-            <Route path="/tabs/changePwd" component={ChangePwd} exact={true}></Route>
-            <Route path="/tabs/supervisorOrg/query" component={SuperVisorOrgQuery} exact={true} />
-            <Route path="/tabs/supervisorOrg/detail" component={SuperVisorOrgDetail} exact={true} />
-            <Route path="/tabs/supervisorOrg/edit" component={SuperVisorOrgEdit} exact={true} />
-            <Route path="/tabs/black/query" component={Black} exact={true} />
-            {/* <Route path="/tabs" render={() => <Redirect to="/tabs/feed" />} exact={true} /> */}
-            <Route path="/tabs/baseInfo/query" component={BaseInfo} exact={true} />
-            <Route path="/tabs/complaint/query" component={Complaint} exact={true} />
-            <Route path="/tabs/complaint/detail" component={ComplaintDetail} exact={true} />
-            <Route path="/tabs/contract/query" component={Contract} exact={true} />
-            <Route path="/tabs/contract/detail" component={ContractDetail} exact={true} />
-            <Route path="/tabs/contractNego/query" component={ContractNego} exact={true} />
-            <Route path="/tabs/lesson/query" component={Lesson} exact={true} />
-            <Route path="/tabs/lesson/detail" component={LessonDetail} exact={true} />
-            <Route path="/tabs/orgMag/query" component={OrgMag} exact={true} />
-            <Route path="/tabs/orgMag/detail" component={OrgMagDetail} exact={true} />
-            <Route path="/tabs/orgMag/edit" component={OrgMagEdit} exact={true} />
-            <Route path="/tabs/teacher/query" component={Teacher} exact={true} />
-            <Route path="/tabs/teacher/detail" component={TeacherDetail} exact={true} />
-            <Route path="/tabs/transfer/query" component={Tranfer} exact={true} />
-            <Route path="/tabs/transfer/detail" component={TranferDetail} exact={true} />
-            <Route path="/tabs/tranferManual/query" component={TranferManul} exact={true} />
-            <Route path="/tabs/contractNego/query" component={ContractNego} exact={true} />
-            <Route path="/tabs/statistics/orgSum" component={OrgSum} exact={true} />
-            <Route path="/tabs/statistics/subject" component={Subject} exact={true} />
-            <Route path="/tabs/statistics/student" component={Student} exact={true} />
-            <Route path="/tabs/statistics/income" component={Income} exact={true} />
-            <Route path="/tabs/statistics/complaint" component={ComplaintStatistic} exact={true} />
-            <Route path="/login" render={() => <Login />} />
-            <Route exact path="/" render={() => <Redirect to="/login" />} />
+              {/* <Route path="/tabs" render={() => <Tabs />} /> */}
+              <Route path="/tabs/announcement/query" component={Announcement} exact={true} />
+              <Route path="/tabs/announcement/detail" component={AnnouncementDetail} exact={true} />
+              <Route path="/tabs/announcement/edit" component={AnnouncementEdit} exact={true} />
+              <Route path="/tabs/attendance/query" component={Attendance} exact={true} />
+              <Route path="/tabs/attendance/detail" component={AttendanceDetail} exact={true} />
+              <Route path="/tabs/changePwd" component={ChangePwd} exact={true}></Route>
+              <Route path="/tabs/supervisorOrg/query" component={SuperVisorOrgQuery} exact={true} />
+              <Route
+                path="/tabs/supervisorOrg/detail"
+                component={SuperVisorOrgDetail}
+                exact={true}
+              />
+              <Route path="/tabs/supervisorOrg/edit" component={SuperVisorOrgEdit} exact={true} />
+              <Route path="/tabs/black/query" component={Black} exact={true} />
+              {/* <Route path="/tabs" render={() => <Redirect to="/tabs/feed" />} exact={true} /> */}
+              <Route path="/tabs/baseInfo/query" component={BaseInfo} exact={true} />
+              <Route path="/tabs/complaint/query" component={Complaint} exact={true} />
+              <Route path="/tabs/complaint/detail" component={ComplaintDetail} exact={true} />
+              <Route path="/tabs/contract/query" component={Contract} exact={true} />
+              <Route path="/tabs/contract/detail" component={ContractDetail} exact={true} />
+              <Route path="/tabs/contractNego/query" component={ContractNego} exact={true} />
+              <Route path="/tabs/lesson/query" component={Lesson} exact={true} />
+              <Route path="/tabs/lesson/detail" component={LessonDetail} exact={true} />
+              <Route path="/tabs/orgMag/query" component={OrgMag} exact={true} />
+              <Route path="/tabs/orgMag/detail" component={OrgMagDetail} exact={true} />
+              <Route path="/tabs/orgMag/edit" component={OrgMagEdit} exact={true} />
+              <Route path="/tabs/teacher/query" component={Teacher} exact={true} />
+              <Route path="/tabs/teacher/detail" component={TeacherDetail} exact={true} />
+              <Route path="/tabs/transfer/query" component={Tranfer} exact={true} />
+              <Route path="/tabs/transfer/detail" component={TranferDetail} exact={true} />
+              <Route path="/tabs/tranferManual/query" component={TranferManul} exact={true} />
+              <Route path="/tabs/contractNego/query" component={ContractNego} exact={true} />
+              <Route path="/tabs/statistics/orgSum" component={OrgSum} exact={true} />
+              <Route path="/tabs/statistics/subject" component={Subject} exact={true} />
+              <Route path="/tabs/statistics/student" component={Student} exact={true} />
+              <Route path="/tabs/statistics/income" component={Income} exact={true} />
+              <Route
+                path="/tabs/statistics/complaint"
+                component={ComplaintStatistic}
+                exact={true}
+              />
+              <Route path="/login" render={() => <Login />} />
+              <Route exact path="/" render={() => <Redirect to="/login" />} />
             </IonRouterOutlet>
           </IonSplitPane>
         </IonReactRouter>
