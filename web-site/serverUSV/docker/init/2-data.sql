3f2a7406d80152e2a5a8d417a30be761030d7192
zhmouohz <zhmouohz@163.com>
2022-07-04 09:24:45 +0800

    增加课程图片

--- web-site/serverUSV/docker/init/2-data.sql
+++ web-site/serverUSV/docker/init/2-data.sql
@@ -5,9 +5,9 @@ INSERT INTO yadadb.t_b_edu_org (edu_id,edu_logo,edu_name,edu_address,edu_legal_p
 INSERT INTO yadadb.t_b_edu_teacher (teacher_id,teacher_name,teacher_identity_no,teacher_field,teacher_experience,teacher_introduce,teacher_rating,teacher_create_date,teacher_create_time,teacher_update_date,teacher_update_time) VALUES
 	 ('teacher00001','马老师','2310000000000','english,math',5,'马老师的介绍内容测试',8.9,'20220630','000000',NULL,NULL);
 -- 插入课程
-INSERT INTO yadadb.t_b_edu_lesson (lesson_id,lesson_name,lesson_total_quantity,lesson_per_price,lesson_total_price,lesson_type,lesson_introduce,lesson_imgs,lesson_outline,lesson_start_date,lesson_start_time,lesson_end_date,lesson_end_time,lesson_status,lesson_create_date,lesson_create_time,lesson_update_date,lesson_update_time,lesson_update_reason,edu_id,teacher_id,lesson_accumulation_quantity) VALUES
-	 ('lesson00001','数学课',100,120,12000,'math','课程介绍测试内容',NULL,'课程大纲测试内容','20220630','000000','20230630','235959','pending','20220630','000000',NULL,NULL,NULL,'edu0001','teacher00001',0),
-	 ('lesson00002','英语入门兴趣班',10,50,500,'english','英语培训班介绍',NULL,'英语兴趣班大纲','20220630','000000','20220830','000000','on','20220630','000000',NULL,NULL,NULL,'edu0001','teacher00001',4);
+INSERT INTO yadadb.t_b_edu_lesson (lesson_images,lesson_id,lesson_name,lesson_total_quantity,lesson_per_price,lesson_total_price,lesson_type,lesson_introduce,lesson_imgs,lesson_outline,lesson_start_date,lesson_start_time,lesson_end_date,lesson_end_time,lesson_status,lesson_create_date,lesson_create_time,lesson_update_date,lesson_update_time,lesson_update_reason,edu_id,teacher_id,lesson_accumulation_quantity) VALUES
+	 ("http://placekitten.com/g/200/300",'lesson00001','数学课',100,120,12000,'math','课程介绍测试内容',NULL,'课程大纲测试内容','20220630','000000','20230630','235959','pending','20220630','000000',NULL,NULL,NULL,'edu0001','teacher00001',0),
+	 ("http://placekitten.com/g/200/300",'lesson00002','英语入门兴趣班',10,50,500,'english','英语培训班介绍',NULL,'英语兴趣班大纲','20220630','000000','20220830','000000','on','20220630','000000',NULL,NULL,NULL,'edu0001','teacher00001',4);
 
 -- 插入消费者
 INSERT INTO yadadb.t_b_consumer (consumer_login_name,consumer_password,consumer_name,consumer_phone,consumer_identity_no,consumer_birthday,consumer_gender) VALUES
