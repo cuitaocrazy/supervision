e0ecb5636e13c656ac705fe2893d261cd0090b39
Kong Li <1263039973@qq.com>
2022-07-05 13:24:15 +0800

    页面添加滚动效果

--- web-site/eduMag/components/pages/OrderQuery.tsx
+++ web-site/eduMag/components/pages/OrderQuery.tsx
@@ -86,7 +86,7 @@ const OrderQuery = () => {
   useEffect(onQuery, []);
   return (
     <IonPage className="bg-gray-100">
-      <div className="relative w-full mx-6 overflow-y-auto">
+      <div className="relative w-full h-screen mx-6 overflow-auto">
         <div className="flex pt-2 my-2 text-gray-800">
           <div className="mr-2 text-gray-600">
             <svg
