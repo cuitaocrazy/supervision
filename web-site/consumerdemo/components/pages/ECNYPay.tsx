556a9b22890901d77d5e8bdbc92c8bdeee076bb7
Kong Li <1263039973@qq.com>
2022-08-16 17:01:30 +0800

    修改完成数币支付页面、支付结果按钮，使其居中对齐的

--- web-site/consumerdemo/components/pages/ECNYPay.tsx
+++ web-site/consumerdemo/components/pages/ECNYPay.tsx
@@ -1,96 +1,104 @@
-import React,{ useEffect,useState,useContext} from "react";
-import { IonPage,IonHeader,IonContent } from "@ionic/react";
-import {useHistory} from 'react-router-dom'
-import Navbar from '../Navbar'
-import {Contract} from '../../types/types'
-import {preOrderURL} from '../../const/const'
-import {AppContext} from '../../appState';
-import { io } from 'socket.io-client'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState, useContext } from "react";
+import { IonPage, IonHeader, IonContent } from "@ionic/react";
+import { useHistory } from "react-router-dom";
+import Navbar from "../Navbar";
+import { Contract } from "../../types/types";
+import { preOrderURL } from "../../const/const";
+import { AppContext } from "../../appState";
+import { io } from "socket.io-client";
+import { Link } from "react-router-dom";
 
 // 数币支付页面
-const ECNYPay =()=>{
-  const socketUrl = 'http://localhost:3003'
+const ECNYPay = () => {
+  const socketUrl = "http://localhost:3003";
   const history = useHistory();
-  const socket = io(socketUrl)
-  const [contract, setContract ] = useState({} as Contract);
+  const socket = io(socketUrl);
+  const [contract, setContract] = useState({} as Contract);
   const { state } = useContext(AppContext);
-  const [payUrl,setPayUrl] = useState("")
-  useEffect(()=>{
-    socket.on('open', () => {
-      console.log('socket io is open !')
-    })
+  const [payUrl, setPayUrl] = useState("");
+  useEffect(() => {
+    socket.on("open", () => {
+      console.log("socket io is open !");
+    });
     fetch(preOrderURL, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-          lessonId:state.lessonDetail.lessonId,
-          username:state.loginUser?.username,
-          studentName:state.studentName,
+        lessonId: state.lessonDetail.lessonId,
+        username: state.loginUser?.username,
+        studentName: state.studentName,
       }),
       headers: {
-        'Content-type': 'application/json;charset=UTF-8',
+        "Content-type": "application/json;charset=UTF-8",
       },
-    }).then(res => res.json())
-    .then((json) => {
-      setContract(json.result)
-      setPayUrl(json.payUrl)
-      socket.emit('pay', json.result.contractId)
-      socket.on(json.result.contractId + '_pay', () => {
-        console.log('支付成功')
-        history.push('/tabs/payResult')
-      })
     })
-      },[])
+      .then((res) => res.json())
+      .then((json) => {
+        setContract(json.result);
+        setPayUrl(json.payUrl);
+        socket.emit("pay", json.result.contractId);
+        socket.on(json.result.contractId + "_pay", () => {
+          console.log("支付成功");
+          history.push("/tabs/payResult");
+        });
+      });
+  }, []);
 
   // const onClick = ()=>{
   //   window.open(payUrl)
   // }
 
-  return <IonPage>
-    <IonHeader>
-      <Navbar title="数币支付页面" />
-    </IonHeader>
-    <IonContent>
-      <div>
-        <div className="px-4 py-4 mx-2 my-2 text-base leading-7 rounded-md shadow-md">
-          <p>
-            <span className="pr-3 text-gray-400">订单编号</span>
-            <span className="text-gray-800">{contract.orderNo}</span>
-          </p>
-          <p>
-            <span className="pr-3 text-gray-400">机构名称</span>
-            <span className="text-gray-800">{contract.eduName}</span>
-          </p>
-          <p>
-            <span className="pr-3 text-gray-400">课程名称</span>
-            <span className="text-gray-800">{contract.lessonName}</span>
-          </p>
-          <p>
-            <span className="pr-3 text-gray-400">课程数量</span>
-            <span className="text-gray-800">{contract.lessonTotalQuantity}</span>
-          </p>
-          {/* <p>
+  return (
+    <IonPage>
+      <IonHeader>
+        <Navbar title="数币支付页面" />
+      </IonHeader>
+      <IonContent>
+        <div>
+          <div className="px-4 py-4 mx-2 my-2 text-base leading-7 rounded-md shadow-md">
+            <p>
+              <span className="pr-3 text-gray-400">订单编号</span>
+              <span className="text-gray-800">{contract.orderNo}</span>
+            </p>
+            <p>
+              <span className="pr-3 text-gray-400">机构名称</span>
+              <span className="text-gray-800">{contract.eduName}</span>
+            </p>
+            <p>
+              <span className="pr-3 text-gray-400">课程名称</span>
+              <span className="text-gray-800">{contract.lessonName}</span>
+            </p>
+            <p>
+              <span className="pr-3 text-gray-400">课程数量</span>
+              <span className="text-gray-800">
+                {contract.lessonTotalQuantity}
+              </span>
+            </p>
+            {/* <p>
             <span className="pr-3 text-gray-400">优惠金额</span>
             <span className="text-gray-800">183.00</span>
             <span className="text-gray-800">元</span>
           </p> */}
-          <hr className="my-4 text-gray-300" />
-          <p className="font-bold text-right text-gray-800">
-            <span className="pr-1">实付金额:</span>
-            <span>¥</span>
-            <span>{contract.lessonTotalPrice}</span>
-          </p>
-        </div>
-        <div className="flex pt-10">
-        <Link to="/eCNYPayResult">
-          <button  className="w-full py-2 mx-6 font-bold tracking-wider text-white shadow-md rounded-3xl shadow-primary-600 bg-primary-600"  
-          value="去支付"  >去支付</button>
-        </Link>
-        </div>
-      </div>
-    </IonContent>
-  </IonPage>
+            <hr className="my-4 text-gray-300" />
+            <p className="font-bold text-right text-gray-800">
+              <span className="pr-1">实付金额:</span>
+              <span>¥</span>
+              <span>{contract.lessonTotalPrice}</span>
+            </p>
+          </div>
 
-}
+          <div className="flex mt-12 text-base">
+            <Link to="/eCNYPayResult" className="flex w-full h-10 py-2">
+              <input
+                className="w-full h-10 py-2 mx-6 font-bold tracking-widest text-white shadow-md bg-primary-600 rounded-3xl bg-grimary-600 shadow-primary-600 focus:bg-primary-700"
+                type="submit"
+                value="去支付"
+              />
+            </Link>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
 
-export default ECNYPay
+export default ECNYPay;
--- web-site/consumerdemo/components/pages/ECNYPayResult.tsx
+++ web-site/consumerdemo/components/pages/ECNYPayResult.tsx
@@ -1,42 +1,56 @@
-import React from 'react';
-import { IonPage, IonHeader, IonContent } from "@ionic/react"
-import { motion } from 'framer-motion'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import {Link} from 'react-router-dom'
-import Navbar from '../Navbar'
+import React from "react";
+import { IonPage, IonHeader, IonContent } from "@ionic/react";
+import { motion } from "framer-motion";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Link } from "react-router-dom";
+import Navbar from "../Navbar";
 
 type FormData = {
   name: string;
-}
+};
 
 // 数币系统的支付结果页面
 const ECNYPayResult = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
     // router.push('/searchLessonList')
-  }
-  return <IonPage>
-    <IonHeader>
-      <Navbar title='支付结果' />
-    </IonHeader>
-    <IonContent>
-      <div className='relative '>
-        <div className='grid justify-center grid-rows-1'>
-          <div className='mt-20 '>
-            <img className='w-48 h-32 rounded-lg' src="http://placekitten.com/g/200/300"></img>
+  };
+  return (
+    <IonPage>
+      <IonHeader>
+        <Navbar title="支付结果" />
+      </IonHeader>
+      <IonContent>
+        <div className="relative ">
+          <div className="grid justify-center grid-rows-1">
+            <div className="mt-20 ">
+              <img
+                className="w-48 h-32 rounded-lg"
+                src="http://placekitten.com/g/200/300"
+              ></img>
+            </div>
+            <div className="pt-4 pb-16 text-lg text-center text-gray-700">
+              支付成功！
+            </div>
           </div>
-          <div className='pt-4 pb-16 text-lg text-center text-gray-700'>支付成功！</div>
-        </div>
-        <div className='flex mx-3'>
-          <Link to='payResult'>
-          <button className='w-full py-2 mx-6 text-base font-bold text-white shadow-md shadow-primary-500 bg-primary-500 rounded-3xl'
-                 >返回教育机构</button>
-          </Link>
 
+          <div className="flex mt-12 text-base">
+            <Link to="/payResult" className="flex w-full h-10 py-2">
+              <input
+                className="w-full h-10 py-2 mx-6 font-bold tracking-widest text-white shadow-md bg-primary-600 rounded-3xl bg-grimary-600 shadow-primary-600 focus:bg-primary-700"
+                type="submit"
+                value="返回教育机构"
+              />
+            </Link>
+          </div>
         </div>
-      </div>
-    </IonContent>
-  </IonPage>
-}
+      </IonContent>
+    </IonPage>
+  );
+};
 
-export default ECNYPayResult
+export default ECNYPayResult;
