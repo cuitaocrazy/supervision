7798459809122cca988573da6afa26b264f64c08
fengming.jiang <fengming.jiang@bjyada.com>
2022-08-31 12:52:28 +0800

    脚本增加启动及等待提示

--- web-site/demo.sh
+++ web-site/demo.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 MODE=$1
 
+function waitStartComplete() {
+    local parameter=$1
+    local name
+    local url
+    if [ "${parameter}" == "consumer" ]; then
+        name="消费者"
+        url="http://localhost:8080/consumer"
+    elif [ "${parameter}" == "edu" ]; then
+        name="教育端"
+        url="http://localhost:8080/edu"
+    elif [ "${parameter}" == "edb" ]; then
+        name="监管端"
+        url="http://localhost:8080/edb"
+    else
+        echo "error parameter, [ consumer | edu | edb ]"
+        exit 1
+    fi
+    local i=0
+    local max=10
+    while [ $i -lt $max ]; do
+
+        local code=$(curl -o /dev/null -s -w "%{http_code}\n" $url)
+        if [ "$code" == "200" ]; then
+            echo "$name 启动成功: $url"
+            i=$(expr $i + $max)
+        else
+            echo "$name 启动未完成: $code , 5秒后重试"
+            sleep 5
+            i=$(expr $i + 1)
+        fi
+    done
+}
+
 function start() {
     echo "start all demo"
     docker-compose -f ./docker/docker-compose-demo.yaml up -d --remove-orphans
-    echo "消费者端: http://localhost:8080/consumer"
-    echo "教育机构端: http://localhost:8080/edu"
-    echo "监管端: http://localhost:8080/edb"
+    waitStartComplete consumer
+    waitStartComplete edu
+    waitStartComplete edb
 }
 
 function cleanWebModule() {
