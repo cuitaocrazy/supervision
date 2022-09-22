01659f486440efa8ee9d8f00dc8154f3c3c1810e
Kong Li <1263039973@qq.com>
2022-09-22 11:16:12 +0800

    修复当退款金额和划拨金额为空值时，页面和控制台报错的问题

--- web-site/regulator-mag-ionic/components/pages/statistics/SupervisorAccount.tsx
+++ web-site/regulator-mag-ionic/components/pages/statistics/SupervisorAccount.tsx
@@ -156,7 +156,7 @@ export const SupervisorAccount: React.FC = () => {
             />
             <TranSumCard
               tranDesc="今日退课金额（元）"
-              tranCount={refundAmt}
+              tranCount={refundAmt? refundAmt.replace('-', ''):"0"}
               d="M9 7.5l3 4.5m0 0l3-4.5M12 12v5.25M15 12H9m6 3H9m12-3a9 9 0 11-18 0 9 9 0 0118 0z"
               logoClassName="w-16 h-16 text-green-400"
             />
@@ -168,7 +168,7 @@ export const SupervisorAccount: React.FC = () => {
             />
             <TranSumCard
               tranDesc="今日划拨金额（元）"
-              tranCount={transferAmt.replaceAll('-', '')}
+              tranCount={transferAmt? transferAmt.replace('-', ''):"0"}
               d="M9 7.5l3 4.5m0 0l3-4.5M12 12v5.25M15 12H9m6 3H9m12-3a9 9 0 11-18 0 9 9 0 0118 0z"
               logoClassName="w-16 h-16 text-orange-400"
             />
